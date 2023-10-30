c964bdf565fc9474f343b54212c5772c4e90a260
David Ortiz Corchero <david140303@hotmail.es>
2023-10-30 16:31:56 +0100

    Cabecera y pie de pagina

--- Pokedex/src/app/app.module.ts
+++ Pokedex/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { PieDePaginaComponent } from './pie-de-pagina/pie-de-pagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    PieDePaginaComponent
   ],
   imports: [
     BrowserModule,
