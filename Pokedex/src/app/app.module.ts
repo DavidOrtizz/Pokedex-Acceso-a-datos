9ff69a50569dcc7d543d7835920e22eec52f4f4e
David Ortiz Corchero <david140303@hotmail.es>
2023-10-30 16:35:43 +0100

    Creacion de interior y añadirlo a app.component.html

--- Pokedex/src/app/app.module.ts
+++ Pokedex/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { PieDePaginaComponent } from './pie-de-pagina/pie-de-pagina.component';
+import { InteriorComponent } from './interior/interior.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceraComponent,
-    PieDePaginaComponent
+    PieDePaginaComponent,
+    InteriorComponent
   ],
   imports: [
     BrowserModule,
