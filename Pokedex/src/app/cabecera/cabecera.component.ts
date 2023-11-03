91eb8d9febc68694cb9f1305aa4ae9b80e715a61
David Ortiz Corchero <david140303@hotmail.es>
2023-11-03 21:13:04 +0100

    Mejoras
    Implementación del icono, mejoras de la cabecera

--- Pokedex/src/app/cabecera/cabecera.component.ts
+++ Pokedex/src/app/cabecera/cabecera.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cabecera',
   templateUrl: './cabecera.component.html',
   styleUrls: ['./cabecera.component.css', '../app.component.css']
 })
-export class CabeceraComponent {
+export class CabeceraComponent implements OnInit {
 
+  menuAbierto = false;
+
+  @Input()
+  buscadorPokemon = '';
+
+  @Output()
+  buscamos = new EventEmitter();
+
+  constructor(){}
+
+  ngOnInit(): void {
+      
+  }
+
+  abrirMenu(){
+   if(this.menuAbierto == true){
+    this.menuAbierto = false;
+   } else {
+    this.menuAbierto = true;
+   }
+  }
 }
