12dfdaf447f4f246eed9460f0e656961b57a5722
David Ortiz Corchero <david140303@hotmail.es>
2023-11-06 17:39:51 +0100

    Arreglos

--- Pokedex/src/app/app.module.ts
+++ Pokedex/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
@@ -21,12 +20,9 @@ import { PruebasComponent } from './pruebas/pruebas.component';
   ],
   imports: [
     BrowserModule,
-
     AppRoutingModule,
     FormsModule,
-
     HttpClientModule
-
   ],
   providers: [],
   bootstrap: [AppComponent]  
