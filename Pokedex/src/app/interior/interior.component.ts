130fb71b41493f853b7bce7f1e8c4ccebf50aca1
AlejandroMorenoGarrido <amorgar539@g.educaand.es>
2023-11-08 17:05:42 +0100

    arreglos para buscador y filtros (Tras usar cambio de generaciones)

--- Pokedex/src/app/interior/interior.component.ts
+++ Pokedex/src/app/interior/interior.component.ts
@@ -20,11 +20,12 @@ export class InteriorComponent implements OnInit {
   // Variable para almacenar el nombre del Pokémon a buscar
 
   @Output() 
-  buscadorPokemon = ''; 
-  opcionSeleccionada: string = '';
+  buscadorPokemon: string = '';
+  busquedaActual: string = ''; 
   generacionSeleccionada: number = 0;
   menuAbierto = false;
   oscuro = false;
+  opcionSeleccionada: number = 0; 
   /*
   shiny= false;
   */
@@ -56,91 +57,49 @@ export class InteriorComponent implements OnInit {
   }
 
     // Método para buscar el Pokémon y emitir el evento
-  buscarPokemon() {
-      // Convierte el nombre a minúsculas para que la búsqueda sea insensible a mayúsculas y minúsculas
+    buscarPokemon() {
       const nombreBuscado = this.buscadorPokemon.toLowerCase();
-      // Filtra los Pokémon cuyo nombre coincide con la búsqueda
+      this.busquedaActual = nombreBuscado; // Almacena la búsqueda actual
       this.pokemons = this.pokemonsBuscador.filter(pokemon => pokemon.nombre.toLowerCase().includes(nombreBuscado));
-
     }
-  filtrarPorTipos() {
-    if (this.tiposSeleccionados.length === 0) {
-      // Si no hay tipos seleccionados, mostrar todos los Pokémon originales
-      this.pokemons = this.pokemonsBuscador;
-    } else {
-      // Filtrar los Pokémon según los tipos seleccionados(viendo si alguno de los elementos coincide)
-      this.pokemons = this.pokemonsBuscador.filter(pokemon => pokemon.tipos.some(tipo => this.tiposSeleccionados.includes(tipo))
-      );
+    filtrarPorTipos() {
+      // Filtra los Pokémon solo si hay tipos seleccionados
+      if (this.tiposSeleccionados.length > 0) {
+        this.pokemons = this.pokemonsBuscador.filter(pokemon => 
+          pokemon.tipos.some(tipo => this.tiposSeleccionados.includes(tipo))
+        );
+      } else {
+        // Si no hay tipos seleccionados, muestra todos los Pokémon originales
+        this.pokemons = this.pokemonsBuscador;
+      }
     }
-  }
 
     filtroTipos(tipo: string) {
       if (this.tiposSeleccionados.includes(tipo)) {
-        // Si el tipo ya está seleccionado, quitarlo de la lista
+        // Si el tipo ya está seleccionado, quítalo de la lista
         this.tiposSeleccionados = this.tiposSeleccionados.filter(t => t !== tipo);
       } else {
-        // Si el tipo no está seleccionado, agregarlo a la lista
+        // Si el tipo no está seleccionado, agrégalo a la lista
         this.tiposSeleccionados.push(tipo);
-      }
-      //volver a la version inicial del array usando el condicional de filtrarPorTipos
+      }     
+      // Realiza el filtrado solo cuando se selecciona o quita un tipo
       this.filtrarPorTipos();
     }
 
     
-      cambiarGeneracion() {
-        
-      
-        // Puedes realizar acciones adicionales en función de la generacion seleccionada
-        if (this.opcionSeleccionada === '1') {
-          this.pokemonService.getPokemons(151).subscribe((dataPokemons: Pokemon[]) => {
-            this.pokemons = dataPokemons;
-            this.pokemonsBuscador = dataPokemons;
-            console.log('Opción:', this.opcionSeleccionada);
-          });
-        } else if (this.opcionSeleccionada === '2') {
-          
-          this.pokemonService.getPokemons(251).subscribe((dataPokemons: Pokemon[]) => {
-            this.pokemons = dataPokemons;
-            this.pokemonsBuscador = dataPokemons;
-          });
-        } else if (this.opcionSeleccionada === '3') {
-          this.pokemonService.getPokemons(386).subscribe((dataPokemons: Pokemon[]) => {
-            this.pokemons = dataPokemons;
-            this.pokemonsBuscador = dataPokemons;
-          });
-        }else if (this.opcionSeleccionada === '4') {
-          this.pokemonService.getPokemons(493).subscribe((dataPokemons: Pokemon[]) => {
-            this.pokemons = dataPokemons;
-            this.pokemonsBuscador = dataPokemons;
-          });
-        }else if (this.opcionSeleccionada === '5') {
-          this.pokemonService.getPokemons(649).subscribe((dataPokemons: Pokemon[]) => {
-            this.pokemons = dataPokemons;
-            this.pokemonsBuscador = dataPokemons;
-          });
-        }else if (this.opcionSeleccionada === '6') {
-          this.pokemonService.getPokemons(721).subscribe((dataPokemons: Pokemon[]) => {
-            this.pokemons = dataPokemons;
-            this.pokemonsBuscador = dataPokemons;
-            
-          });
-        }else if (this.opcionSeleccionada === '7') {
-          this.pokemonService.getPokemons(809).subscribe((dataPokemons: Pokemon[]) => {
-            this.pokemons = dataPokemons;
-            this.pokemonsBuscador = dataPokemons;
-          });
-        }else if (this.opcionSeleccionada === '8') {
-          this.pokemonService.getPokemons(898).subscribe((dataPokemons: Pokemon[]) => {
-            this.pokemons = dataPokemons;
-            this.pokemonsBuscador = dataPokemons;
-          });
-        }else if (this.opcionSeleccionada === '9') {
-          this.pokemonService.getPokemons(1017).subscribe((dataPokemons: Pokemon[]) => {
-            this.pokemons = dataPokemons;
-            this.pokemonsBuscador = dataPokemons;
-          });
+    cambiarGeneracion() {
+      this.pokemonService.getPokemons(this.opcionSeleccionada).subscribe((dataPokemons: Pokemon[]) => {
+        this.pokemons = dataPokemons;
+        this.pokemonsBuscador = dataPokemons;
+    
+        // Verifica si había una búsqueda activa
+        if (this.busquedaActual) {
+          this.pokemons = this.pokemons.filter(pokemon => pokemon.nombre.toLowerCase().includes(this.busquedaActual));
         }
-      
+    
+        // Llama a la función filtrarPorTipos para aplicar el filtro de tipos
+        this.filtrarPorTipos();
+      });
     }
     /*
     cambiarImagen() {
