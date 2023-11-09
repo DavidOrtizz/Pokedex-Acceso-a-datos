76fb65804f94aee2db6fc38d29ae49b4ddb9f818
AlejandroMorenoGarrido <amorgar539@g.educaand.es>
2023-11-09 17:09:00 +0100

    cambios post virus

---
+++ Pokedex/src/app/datos-pokemon/datos-pokemon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-datos-pokemon',
+  templateUrl: './datos-pokemon.component.html',
+  styleUrls: ['./datos-pokemon.component.css']
+})
+export class DatosPokemonComponent {
+
+}
