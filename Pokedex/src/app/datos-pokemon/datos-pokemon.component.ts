efde9a90faf65a890dba999961fcb818e034fb06
David Ortiz Corchero <david140303@hotmail.es>
2023-11-20 17:33:04 +0100

    Css y botones de los datos pokemon implementado

--- Pokedex/src/app/datos-pokemon/datos-pokemon.component.ts
+++ Pokedex/src/app/datos-pokemon/datos-pokemon.component.ts
@@ -1,43 +1,66 @@
-import { Component, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PokemonService } from '../pokemon.service';
-import { EventEmitter, Input, OnInit} from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Pokemon } from '../pokemon';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PokemonDetalle } from '../pokemonDetalle';
 
 @Component({
-selector: 'app-detalle',
-templateUrl: './datos-pokemon.component.html',
-styleUrls: ['./datos-pokemon.component.css']
+  selector: 'app-detalle',
+  templateUrl: './datos-pokemon.component.html',
+  styleUrls: ['./datos-pokemon.component.css']
 })
 export class DatosPokemonComponent implements OnInit {
-      pokemon: PokemonDetalle = {
-      id: 0,
-      nombre: "",
-      tipos: [],
-      imagen:"",
-      shiny: "",
-      peso:0,    
-      vida:0,
-      ataque:0,
-      defensa:0,
-      atqEspecial:0,
-      defEspecial:0,
-      velocidad:0,
-      altura:0,
-      descripcion:""
-    };
+  pokemon: PokemonDetalle = {
+    id: 0,
+    nombre: "",
+    tipos: [],
+    imagen: "",
+    shiny: "",
+    peso: 0,
+    vida: 0,
+    ataque: 0,
+    defensa: 0,
+    atqEspecial: 0,
+    defEspecial: 0,
+    velocidad: 0,
+    altura: 0,
+    descripcion: ""
+  };
 
-constructor(
-private PokemonService: PokemonService, private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private pokemonService: PokemonService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) { }
 
-ngOnInit(): void {
-const id = this.activatedRoute.snapshot.paramMap.get('id') as unknown as number;
-this.PokemonService.getPokemonsDetalle(id).subscribe((pokemonDatos: PokemonDetalle) => {
-    this.pokemon = pokemonDatos;
+  ngOnInit(): void {
+    this.cargarDetalles();
+  }
 
-  });
+  cargarDetalles() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id') as unknown as number;
+    this.idPokemon(id);
+    this.pokemonService.getPokemonsDetalle(id).subscribe((pokemonDatos: PokemonDetalle) => {
+      this.pokemon = pokemonDatos;
+    });
+  }
 
+  idPokemon(id: number) {
+    this.router.navigate(['/datos/', id]);
+  }
 
-}
+  anteriorPokemon() {
+    if ((this.pokemon.id - 1) != 0) { // Para controlar a que no llegue a pokemon 0
+      this.idPokemon(this.pokemon.id - 1);
+      setTimeout(() => {
+        location.reload();
+      }, 1);
+    }
+  }
+
+  siguientePokemon() {
+    this.idPokemon(this.pokemon.id + 1);
+    setTimeout(() => {
+      location.reload();
+    }, 1);
+  }
 }
